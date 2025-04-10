1ca6282b217b65843022c598ed64d76c77646fc4
Alisa Beztsinna <23imc10600@fh-krems.ac.at>
2025-04-10 10:34:03 +0200

    added more explanations

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import os
 coco_annotations_file="Solar Panel Fault Dataset.v1i.coco/train/_annotations.coco.json"
 coco_images_dir="Solar Panel Fault Dataset.v1i.coco/train"
 coco= COCOParser(coco_annotations_file, coco_images_dir)
+print(coco.cat_dict.values())  # all classes contained in the dataset
 
-print(coco.cat_dict.values())#all of classes contained in the dataset
-# define a list of colors for drawing bounding boxes
+
 def assign_colour(class_name):
+    """Function returns the colour based on the category."""
     if class_name == "Physical Damage":
         return "purple"
     elif class_name =="Bird Drop":
@@ -25,16 +26,18 @@ def assign_colour(class_name):
         return "orange"
     elif class_name == "Snow":
         return "pink"
-color_list = ["pink", "red", "teal", "blue", "orange", "yellow", "magenta","blue"]
+
+
 num_imgs_to_disp = 4
 total_images = len(coco.get_imgIds())  # total number of images
-sel_im_idxs = np.random.permutation(total_images)[:num_imgs_to_disp]#randomly select the indexes of images to display
+sel_im_idxs = np.random.permutation(total_images)[:num_imgs_to_disp]  # randomly select the indexes of images to display
 img_ids = coco.get_imgIds()
-selected_img_ids = [img_ids[i] for i in sel_im_idxs]#select images with chosen early random indexes
-ann_ids = coco.get_annIds(selected_img_ids)#select annotations
-im_licenses = coco.get_imgLicenses(selected_img_ids)#select licenses
-fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(15, 10))#creates a 2×2 grid of subplots to plot four images in a single figure.
+selected_img_ids = [img_ids[i] for i in sel_im_idxs]  # select images with chosen early random indexes
+ann_ids = coco.get_annIds(selected_img_ids)  # select annotations
+im_licenses = coco.get_imgLicenses(selected_img_ids)  # select licenses
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(15, 10))  # creates a 2×2 grid of subplots to plot four images
 ax = ax.ravel()
+
 for i, im in enumerate(selected_img_ids):
     im_info = coco.im_dict[im]
     file_name = im_info['file_name']
@@ -44,15 +47,14 @@ for i, im in enumerate(selected_img_ids):
     annotations = coco.load_anns(ann_ids)
     for ann in annotations:
         bbox = ann['bbox']
-        x, y, w, h = [int(b) for b in bbox]
+        x, y, w, h = [int(b) for b in bbox]  # coordinates of bounding box
         class_id = ann["category_id"]
         class_name = coco.load_cats(class_id)[0]["name"]
         license = coco.get_imgLicenses(im)[0]["name"]
         color_ = assign_colour(class_name)
-        print(class_name)
-        rect = plt.Rectangle((x, y), w, h, linewidth=2, edgecolor=color_, facecolor='none')
+        rect = plt.Rectangle((x, y), w, h, linewidth=2, edgecolor=color_, facecolor='none')  # creates a rectangle based on the extracted coordinates and assigned colour
         t_box = ax[i].text(x, y, class_name, color='red', fontsize=10)
-        t_box.set_bbox(dict(boxstyle='square, pad=0', facecolor='white', alpha=0.6, edgecolor='blue'))
+        t_box.set_bbox(dict(boxstyle='square, pad=0', facecolor='white', alpha=0.6, edgecolor='blue'))  # creates box with the name of the category
         ax[i].add_patch(rect)
 
     ax[i].axis('off')
